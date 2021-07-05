1b4a4c256bc2af6763ec33ba639221c2f536947f
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-07-05 16:17:17 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/felipekonig/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipekonig.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
