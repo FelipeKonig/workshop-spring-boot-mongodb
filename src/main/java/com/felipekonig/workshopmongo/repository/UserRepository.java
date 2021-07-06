cd45762e150075eb4e6dace31557455246cf5dec
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-07-06 15:51:45 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/felipekonig/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.felipekonig.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipekonig.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
