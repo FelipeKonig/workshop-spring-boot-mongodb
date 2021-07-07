af8cab71c5f02377af852e76e92212c37df854e4
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-07-07 15:56:39 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/felipekonig/workshopmongo/config/Instantion.java
+++ src/main/java/com/felipekonig/workshopmongo/config/Instantion.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.felipekonig.workshopmongo.domain.Post;
 import com.felipekonig.workshopmongo.domain.User;
+import com.felipekonig.workshopmongo.dto.AuthorDTO;
 import com.felipekonig.workshopmongo.repository.PostRepository;
 import com.felipekonig.workshopmongo.repository.UserRepository;
 
@@ -18,7 +19,7 @@ public class Instantion implements CommandLineRunner {
 
 	@Autowired
 	UserRepository userRepo;
-	
+
 	@Autowired
 	PostRepository postRepo;
 
@@ -38,9 +39,9 @@ public class Instantion implements CommandLineRunner {
 		userRepo.saveAll(Arrays.asList(maria, alex, bob));
 
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
-				maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
+				new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepo.saveAll(Arrays.asList(post1, post2));
 	}
 
